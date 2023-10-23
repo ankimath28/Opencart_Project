8cc50db08bea687ff9ee4886c6e62ab528409014
ankimath28 <ankita.mathankar28@gmail.com>
2023-10-23 08:19:38 +0530

    created project structure

---
+++ open_cartApplication/src/precondition/TestBase.java
@@ -0,0 +1,5 @@
+package precondition;
+
+public class TestBase {
+
+}
---
+++ opencart_project/src/main/java/opencart_project/opencart_project/App.java
@@ -0,0 +1,13 @@
+package opencart_project.opencart_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
